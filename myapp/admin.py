3ecfa4aea4f7d874f7d182022c188f3070f76175
Antoshkin <milka.vilka.007@gmail.com>
2023-11-23 16:59:42 +0200

    Homework 3

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import (Article,
+                     Topic,
+                     Comment)
+import datetime
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ('title', 'text', 'author')
+    list_display = ('title', 'text', 'created_at', 'updated_at', 'author')
+    #
+    # def upper_name(self, obj):
+    #     return obj.name.upper()
+
+admin.site.register(Article, ArticleAdmin)
+
+class TopicAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'publications', 'prefers')
+    list_display = ('title', 'description')
+
+admin.site.register(Topic, TopicAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('message', 'contains', 'author')
+    list_display = ('message', 'created_at', 'contains', 'author')
+
+admin.site.register(Comment, CommentAdmin)
+
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import datetime
 
-# Create your models here.
+User = get_user_model()
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_article', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    publications = models.ManyToManyField(Article)
+    prefers = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    contains = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='contains', null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comment', null=True, blank=True)
+
+    def __str__(self):
+        return self.message
+
+
+
